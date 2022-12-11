3e1ee372005dccdb87643b0c918311742bcc9c38
Dhairya Shah <dtshah24022006@gmail.com>
2022-12-11 16:21:18 +0530

    fixed class to className

--- pages/app/index.js
+++ pages/app/index.js
@@ -233,7 +233,7 @@ export default function Index() {
                               viewBox="0 0 24 24"
                               width="14"
                               height="14"
-                              class="text-gray-700 transition-all group-hover:text-blue-800"
+                              className="text-gray-700 transition-all group-hover:text-blue-800"
                             >
                               <path d="M8 17.929H6c-1.105 0-2-.912-2-2.036V5.036C4 3.91 4.895 3 6 3h8c1.105 0 2 .911 2 2.036v1.866m-6 .17h8c1.105 0 2 .91 2 2.035v10.857C20 21.09 19.105 22 18 22h-8c-1.105 0-2-.911-2-2.036V9.107c0-1.124.895-2.036 2-2.036z"></path>
                             </svg>
@@ -256,9 +256,9 @@ export default function Index() {
                                 <path d="M18 20V4"></path>
                                 <path d="M6 20v-4"></path>
                               </svg>
-                              <p class="whitespace-nowrap text-sm text-gray-500">
+                              <p className="whitespace-nowrap text-sm text-gray-500">
                                 {data.clicks}
-                                <span class="ml-1 hidden sm:inline-block">
+                                <span className="ml-1 hidden sm:inline-block">
                                   clicks
                                 </span>
                               </p>
