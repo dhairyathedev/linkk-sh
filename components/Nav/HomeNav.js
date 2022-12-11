60f4d8d14991ce5f830b1535b9514157f288fbd8
Dhairya Shah <dtshah24022006@gmail.com>
2022-12-11 18:30:24 +0530

    update login status and fixed darkmode bug

--- components/Nav/HomeNav.js
+++ components/Nav/HomeNav.js
@@ -1,37 +1,47 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
+import { supabase } from "../../lib/supabase";
+export default function HomeVav() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [loading, setLoading] = useState(true)
+  useEffect(() => {
+    async function loadSession() {
+      const { data } = await supabase.auth.getUser();
+      if (data) {
+        data.user.id ? setIsAuthenticated(true) : setIsAuthenticated(false);
+        setLoading(false)
+      }
+    }
+    loadSession();
+  }, [isAuthenticated]);
 
-export default function HomeNav() {
   return (
     <nav
-        aria-label="Site Nav"
-        className="mx-auto flex max-w-screen-lg items-center justify-between p-4"
-      >
-        <Link
-          href="/"
-          className="rounded-lg inline-flex items-center space-x-2"
-        >
-          <span className="sr-only">Logo</span>
-          <Image
-            src="/assets/images/logo.svg"
-            width={36}
-            height={36}
-            className="w"
-            alt="logo"
-          />
-          <span className="font-bold text-2xl font-primary">Linkk.sh</span>
-        </Link>
-        <ul className="flex items-center gap-2 text-sm font-medium text-gray-500">
-          <li>
-            <Link
-              href="/app"
-              className="font-secondary rounded-full border border-black bg-black py-1.5 px-5 text-sm text-white transition-all hover:bg-white hover:text-black"
-            >
-              SignIn
-            </Link>
-          </li>
-        </ul>
-      </nav>
-  )
+      aria-label="Site Nav"
+      className="mx-auto flex max-w-screen-lg items-center justify-between p-4"
+    >
+      <Link href="/" className="rounded-lg inline-flex items-center space-x-2">
+        <span className="sr-only">Logo</span>
+        <Image
+          src="/assets/images/logo.svg"
+          width={36}
+          height={36}
+          className="w"
+          alt="logo"
+        />
+        <span className="font-bold text-2xl font-primary">Linkk.sh</span>
+      </Link>
+      <ul className="flex items-center gap-2 text-sm font-medium text-gray-500">
+        <li>
+          <Link
+            href="/app"
+            className="font-secondary rounded-full border border-black bg-black py-1.5 px-5 text-sm text-white transition-all hover:bg-white hover:text-black"
+          >
+             {isAuthenticated || !loading? "Dashboard" :  "SignIn"}
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -18,4 +18,7 @@ module.exports = {
     require("daisyui")
 
   ],
+  daisyui: {
+    themes: ["light"]    
+  },
 }
