1de617b24c615e1d2c1bcdd54f47660fc2acd62a
Dhairya Shah <dtshah24022006@gmail.com>
2022-12-18 18:34:19 +0530

    update login form

--- pages/login.js
+++ pages/login.js
@@ -1,29 +1,35 @@
+import Image from "next/image";
 import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
+import Background from "../components/Background";
 import { supabase } from "../lib/supabase";
 export default function Home() {
   const [email, setEmail] = useState("");
-  const router = useRouter()
+  const router = useRouter();
   useEffect(() => {
     async function loadSession() {
-      const {data: autheticated} = await supabase.auth.getSession()
-      if(autheticated.session !== null){
-        router.push("/app")
+      const { data: autheticated } = await supabase.auth.getSession();
+      if (autheticated.session !== null) {
+        router.push("/app");
       }
     }
     loadSession();
   }, [router]);
   const signInWithEmail = async () => {
     try {
+      if(email){
       const { data, error } = await supabase.auth.signInWithOtp({
         email: email,
       });
-      if (data) {
+      if (data && email) {
         alert("Email has been sent!");
         console.log(data);
       } else if (error) {
-        throw error;
+        alert(error);
       }
+    }else{
+      alert("Please enter email!")
+    }
     } catch (error) {
       throw error;
     }
@@ -40,17 +46,26 @@ export default function Home() {
   };
   return (
     <>
-      <div className="w-full bg-gray-50 min-h-screen">
+    <Background />  
+      <div className="z-10 mx-auto max-w-md px-2.5 sm:max-w-lg sm:px-0 brightness-100 w-full min-h-screen">
         <div className="flex justify-center items-center min-h-screen max-w-screen-sm mx-auto">
-          <div className="w-full bg-white shadow-2xl rounded-md p-2">
+          <div className="w-full bg-white shadow-2xl rounded-md p-4">
             <div className="flex flex-col space-y-2">
               <div className="text-center">
-                <h1 className="text-xl font-bold">SignIn</h1>
-                <p>Hey enter your email address to login</p>
+                <div className="flex justify-center items-center">
+                  <Image
+                    src="/assets/images/logo.svg"
+                    width={36}
+                    height={36}
+                    className="w"
+                    alt="logo"
+                  />
+                </div>
+                <h1 className="text-xl font-bold mt-2">Welcome to linkk.sh</h1>
+                <p>Enter your email address to login</p>
               </div>
               <div>
                 <label className="block">
-                  <span className="text-gray-700">Email</span>
                   <input
                     type="email"
                     className="
@@ -62,14 +77,19 @@ export default function Home() {
                     border-transparent
                     focus:border-gray-500 focus:bg-white focus:ring-0
                   "
-                    placeholder=""
+                    placeholder="john@apple.com"
                     data-ddg-inputtype="identities.email"
                     value={email}
-                    onChange={(e) => {setEmail(e.target.value)}}
+                    onChange={(e) => {
+                      setEmail(e.target.value);
+                    }}
                   />
                 </label>
               </div>
-              <button className="border-black bg-black text-white hover:bg-white hover:text-black flex h-10 w-full items-center justify-center rounded-md border text-sm transition-all focus:outline-none" onClick={signInWithEmail}>
+              <button
+                className="border-black bg-black text-white hover:bg-white hover:text-black flex h-10 w-full items-center justify-center rounded-md border text-sm transition-all focus:outline-none"
+                onClick={signInWithEmail}
+              >
                 <p>Send magic link</p>
               </button>
             </div>
