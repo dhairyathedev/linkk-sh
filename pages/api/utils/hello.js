b530071b45fe32fb51a54d672151f9bd9f62d6cf
Dhairya Shah <dtshah24022006@gmail.com>
2023-01-23 12:37:35 +0530

    update test cron

--- pages/api/utils/hello.js
+++ pages/api/utils/hello.js
@@ -3,9 +3,10 @@ import { checkDateInSupabase } from "../../../lib/date";
 
 async function handler(req, res) {
   console.log("If this is printed, the signature has already been verified");
-  const date = new Date().toLocaleDateString();
+  const date = new Date().toISOString();
     const checkThis = checkDateInSupabase("2023-01-22 18:30:00+00")
     console.log("Date Status: ", checkThis)
+    console.log("Current Date Time: ", date)
   // do stuff
   res.status(200).end();
 }
