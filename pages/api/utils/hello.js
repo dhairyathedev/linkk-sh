9fe6cf9c48503d49cbcb4e2c38b8906a40b63202
Dhairya Shah <dtshah24022006@gmail.com>
2023-01-23 12:27:46 +0530

    update test cron

--- pages/api/utils/hello.js
+++ pages/api/utils/hello.js
@@ -2,7 +2,8 @@ import { verifySignature } from "@upstash/qstash/nextjs";
 
 async function handler(req, res) {
   console.log("If this is printed, the signature has already been verified");
-
+  const date = new Date().toLocaleDateString();
+  console.log(date);
   // do stuff
   res.status(200).end();
 }
